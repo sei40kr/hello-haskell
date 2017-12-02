65648f9529514f4ef16c36d2f898b54a6eb84457
Seong Yong-ju <sei40kr@gmail.com>
2017-12-02 11:39:49 +0900

    Add solution 8-1

---
+++ practice8-1.hs
@@ -0,0 +1,13 @@
+-- practice8-1.hs
+-- author: Seong Yong-ju ( @sei40kr )
+import Control.Applicative
+
+fib 0 = Just 0
+fib 1 = Just 1
+fib n
+  | n > 1 = (+) <$> fib (n - 2) <*> fib (n - 1)
+  | otherwise = Nothing
+
+main = do
+  print $ fib (-1)
+  print $ fib 6
