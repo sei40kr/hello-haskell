86e89246cada3962c08a225a22cb0b42072ef15e
Seong Yong-ju <sei40kr@gmail.com>
2018-07-24 21:22:04 +0900

    Update stref example
    Add type signature.

--- examples/stref.hs
+++ examples/stref.hs
@@ -1,13 +1,15 @@
 -- stref.hs
 -- author: Seong Yong-ju ( @sei40kr )
-import Control.Monad
-import Control.Monad.ST
-import Data.STRef
+import           Control.Monad
+import           Control.Monad.ST
+import           Data.STRef
 
+sum' :: (Foldable t, Num a) => t a -> a
 sum' xs =
   runST $ do
     v <- newSTRef 0
     forM_ xs $ \i -> modifySTRef v (+ i)
     readSTRef v
 
-main = print $ sum' [1 .. 100]
+main :: IO ()
+main = print $ sum' [1 .. (100 :: Integer)]
