0895ed3dd9d712378751e31c2c6efedb8afb63c5
Seong Yong-ju <sei40kr@gmail.com>
2018-07-24 21:20:12 +0900

    Update ioref example
    Add type signature.

--- examples/ioref.hs
+++ examples/ioref.hs
@@ -1,11 +1,13 @@
 -- ioref.hs
 -- author: Seong Yong-ju ( @sei40kr )
-import Control.Monad
-import Data.IORef
+import           Control.Monad
+import           Data.IORef
 
+sum' :: (Foldable t, Num b) => t b -> IO b
 sum' xs = do
   v <- newIORef 0
   forM_ xs $ \i -> modifyIORef v (+ i)
   readIORef v
 
-main = print =<< sum' [1 .. 100]
+main :: IO ()
+main = sum' [1 .. 100 :: Integer] >>= print
