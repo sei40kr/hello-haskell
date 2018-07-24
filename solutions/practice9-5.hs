ecbebe1e10d23a10144bf3b9d55b08c4abe05720
Seong Yong-ju <sei40kr@gmail.com>
2018-07-24 21:59:20 +0900

    Add practice9-5

---
+++ solutions/practice9-5.hs
@@ -0,0 +1,28 @@
+-- practice-9-5.hs
+
+-- author: Seong Yong-ju <sei40kr@gmail.com>
+
+import           Control.Monad.Identity
+import           Control.Monad.Reader
+
+bind :: MonadReader r m => Reader r t -> (t -> Reader r a) -> m a
+bind a b = reader $ \r -> runReader (b (runReader a r)) r
+
+return' :: MonadReader r m => a -> m a
+return' x = reader $ const x
+
+ask' :: ReaderT Integer Identity Integer
+ask' = reader id
+
+local' :: MonadReader t m => (t -> r) -> Reader r a -> m a
+local' f m = reader $ \r -> runReader m $ f r
+
+test :: Integer -> (Integer, Integer, Integer)
+test x =
+  (`runReader` x) $
+  ask' `bind` \a ->
+    local' (+ 1) ( ask' `bind` \b -> return' b) `bind` \b ->
+      ask' `bind` \c -> return' (a, b, c)
+
+main :: IO ()
+main = print $ test 1
