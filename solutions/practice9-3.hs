b5fe4448fb3bed804df907a65dfd6bc3b70050b5
Seong Yong-ju <sei40kr@gmail.com>
2018-07-25 19:26:29 +0900

    Update practice9-3
    Add type signatures.

--- solutions/practice9-3.hs
+++ solutions/practice9-3.hs
@@ -1,20 +1,28 @@
 -- practice9-3.hs
 -- author: Seong Yong-ju ( @sei40kr )
-import Control.Monad
-import Control.Monad.State
+import           Control.Monad
+import           Control.Monad.State
 
-a `bind` b = state $ \s ->
+bind :: State s a1 -> (a1 -> State s a2) -> State s a2
+bind a b = state $ \s ->
     let (r1, s1) = runState a s
         (r2, s2) = runState (b r1) s1
     in  (r2, s2)
-return' x  = state $ \s -> (x , s)
-get'       = state $ \s -> (s , s)
+
+return' :: a -> State s a
+return' x = state $ \s -> (x, s)
+
+get' :: State s s
+get' = state $ \s -> (s, s)
+
+put' :: s -> State s ()
 put'    x  = state $ const ((), x)
 
+fib :: Int -> Int
 fib x =
   (`evalState` (0, 1)) $
-  (replicateM_ (x - 1) $ get' `bind` \(a, b) -> put' (b, a + b)) `bind` \_ ->
+  replicateM_ (x - 1) (get' `bind` \(a, b) -> put' (b, a + b)) `bind` \_ ->
     get' `bind` \v -> return' $ snd v
 
-main = do
-  print $ fib 10
+main :: IO ()
+main = print $ fib 10
