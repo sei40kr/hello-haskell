49de92272a3286ee1f361409481c5b7c6f1fad12
Seong Yong-ju <sei40kr@gmail.com>
2018-08-02 20:16:54 +0900

    Add practice10-1

---
+++ solutions/practice10-1.hs
@@ -0,0 +1,17 @@
+import           Control.Monad.Identity
+import           Control.Monad.State
+
+-- practice9-10.hs
+
+-- author: Seong Yong-ju <sei40kr@gmail.com>
+
+return' :: a -> State s a
+return' x = StateT $ \s -> Identity (x, s)
+
+runState' :: State s a -> (s -> (a, s))
+runState' st = runIdentity . runStateT st
+
+main :: IO ()
+main = do
+  let st = return' 1 :: State s Int
+  print $ runState' st ()
