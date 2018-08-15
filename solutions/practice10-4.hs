2b663d478e18fd13a801cb888606eece520ff0bb
Seong Yong-ju <sei40kr@gmail.com>
2018-08-15 20:39:01 +0900

    Add practice10-4

---
+++ solutions/practice10-4.hs
@@ -0,0 +1,19 @@
+import Data.Char
+
+-- practice10-4.hs --- Maybe monad
+
+-- author: Seong Yong-ju <sei40kr@gmail.com>
+
+getch f (x:xs) | f x = Just (x, xs)
+getch _ _ = Nothing
+
+test s0 = do
+  (ch1, s1) <- getch isUpper s0
+  (ch2, s2) <- getch isLower s1
+  (ch3, s3) <- getch isDigit s2
+  return [ch1, ch2, ch3]
+
+main :: IO ()
+main = do
+  print $ test "Aa0"
+  print $ test "abc"
