6aa9840aad12b27c0de44915db8911d1019fe7a0
Seong Yong-ju <sei40kr@gmail.com>
2018-08-27 19:52:14 +0900

    Add type signatures to practice10-4

--- solutions/practice10-4.hs
+++ solutions/practice10-4.hs
@@ -4,13 +4,15 @@ import Data.Char
 
 -- author: Seong Yong-ju <sei40kr@gmail.com>
 
+getch :: (Char -> Bool) -> String -> Maybe (Char, String)
 getch f (x:xs) | f x = Just (x, xs)
 getch _ _ = Nothing
 
+test :: String -> Maybe String
 test s0 = do
   (ch1, s1) <- getch isUpper s0
   (ch2, s2) <- getch isLower s1
-  (ch3, s3) <- getch isDigit s2
+  (ch3, _) <- getch isDigit s2
   return [ch1, ch2, ch3]
 
 main :: IO ()
