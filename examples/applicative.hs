d230820930782c55677b58b46831d1cb47ca1a16
Seong Yong-ju <sei40kr@gmail.com>
2018-09-04 22:05:39 +0900

    Add reimplementation of <*> to the applicative example

--- examples/applicative.hs
+++ examples/applicative.hs
@@ -2,10 +2,18 @@
 
 -- author: Seong Yong-ju <sei40kr@gmail.com>
 
+-- Reimplementation of <$>
 dollar :: Monad m => (a -> r) -> m a -> m r
 dollar f m = do
   a <- m
   return $ f a
 
+-- Reimplementation of <*>
+asterisk :: Monad m => m (a -> r) -> m a -> m r
+asterisk mf m = do
+  f <- mf
+  a <- m
+  return $ f a
+
 main :: IO ()
-main = print $ (1 +) `dollar` [1::Int]
+main = print $ (+) `dollar` [1] `asterisk` [1::Int]
