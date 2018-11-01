0a29903a665642c6899dc83063ea32fdbcda7f29
Seong Yong-ju <sei40kr@gmail.com>
2018-11-01 19:39:41 +0900

    Format practice11-4

--- solutions/practice11-4.hs
+++ solutions/practice11-4.hs
@@ -1,11 +1,11 @@
-import Control.Exception
-import Control.Monad
+import           Control.Exception
+import           Control.Monad
 
 -- practice11-4.hs
 
 -- author: Seong Yong-ju <sei40kr@gmail.com>
 
 main = do
-    forM_ [0..3] $ \i -> do
-        a <- try $ evaluate $ 6 `div` i
-        print (a :: Either SomeException Int)
+  forM_ [0 .. 3] $ \i -> do
+    a <- try $ evaluate $ 6 `div` i
+    print (a :: Either SomeException Int)
